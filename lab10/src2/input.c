b02de116c35bce59a60fc3d6e302eccb108054b6
Denyyyys <denys.fokashchuk@gmail.com>
2024-04-30 17:52:22 +0200

    Added initial lab10

---
+++ lab10/src2/input.c
@@ -0,0 +1,13 @@
+#include "rlin.h"
+
+void WczytajDane(void)
+{
+        printf("Podaj wspolczynniki rownania liniowego ax + b = 0\n");
+        printf("a = ");
+        scanf("%lf",&a);
+        printf("b = ");
+        scanf("%lf",&b);
+	printf("\n");
+        return;
+}
+
